35262eb891e2ae06c29ec98b79c675245a72c494
darefyeva <98686816+darefyeva@users.noreply.github.com>
2022-11-04 21:06:00 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import os
 import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+
 
 def prepare_data(file_name: str):
     """
@@ -25,34 +25,41 @@ def prepare_data(file_name: str):
 
 def fitting(x_train: pd.DataFrame, y_train: pd.Series, model_name: str, id_model: int, model_params: dict):
     """
-    На вход принимает признаки и таргет, название модели, которую нужно обучить и параменты этой модели.
-    Обучение модели на train.
-    Возвращает обученную модель.
+    На вход принимает признаки и таргет, название модели и id, которую нужно обучить и параменты этой модели.
 
     Param:
-    X_train: pd.DataFrame
+    x_train: pd.DataFrame
     y_train: pd.Series
-    model_name :str
-    model_params :dict
-
-    Return:
-    model
+    model_name: str
+    id_model: int
+    model_params: dict
 
     """
     models = {'RandomForestRegressor': RandomForestRegressor(), 'LinearRegression': LinearRegression()}
     for param in model_params:
         if param not in models[model_name].get_params().keys():
-            return f'Параметр {param} у модели {model_name} не найден',  400
-
+            return 'error', param
     model = models[model_name].set_params(**model_params)
     model.fit(x_train, y_train)
-    with open('../fitted_models/' + str(id_model), 'wb') as file:
+    with open('fitted_models/' + str(id_model) + '.pkl', 'wb') as file:
         pickle.dump(model, file)
+    return 'ok', 1
 
 
-# def prediction(fitted_model, X_test: pd.DataFrame):
-# """
-# """
-#     y_pred = fitted_model.predict(X_test)
-#
-#     return y_pred
+def prediction(id_model: int, x_test: pd.DataFrame):
+    """
+    На вход принимает id модели и тестовую выборку признаков
+
+    Param:
+    x_test: pd.DataFrame
+    id_model: int
+
+    Return:
+    y_pred: pd.Series
+
+    """
+
+    with open('fitted_models/' + str(id_model) + '.pkl', 'rb') as file:
+        fitted_model = pickle.load(file)
+    y_pred = fitted_model.predict(x_test)
+    return y_pred
